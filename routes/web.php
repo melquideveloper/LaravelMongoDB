27faefb2023c556e177f1e486ff2b55d1c078405
melquideveloper <melque20@hotmail.com>
2025-08-15 13:52:10 -0300

    Crud MongoDB

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Usuario;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/teste', function () {
+
+    // $usuario = Usuario::create([
+    //     'nome' => 'Teste',
+    //     'email' => 'teste@email.com',
+    //     'idade' => 25,
+    // ]);
+
+    // Inserir
+    // Usuario::create([
+    //     'nome' => 'João',
+    //     'idade' => 30
+    // ]);
+
+    // Buscar
+    $usuario = Usuario::where('nome', '=', 'Teste')->get();
+    // dd($usuarios);
+
+    // Atualizar
+    // $usuario = Usuario::find($id);
+    $usuario->idade = 35;
+    $usuario->save();
+
+    // Deletar
+    // $usuario->delete();
+
+    return $usuario;
 });
